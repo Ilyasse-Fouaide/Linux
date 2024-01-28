91dc37d182c4d23b0e48d8012ad0d64258a80219
ilyasse-fouaide <ilyasse.fouaide@gmail.com>
2024-01-28 20:21:53 +0100

    updating.
    Co-authored-by: sami-fouaide <sfouaide@gmail.com>

--- bash-scripting/data-streams.sh
+++ bash-scripting/data-streams.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-ls -l &>> log.log
+if [ ! -d ~/Documents/Linux/log/data-stream ]; then
+    echo -e "--- creating log dir for you ... ---"
+    mkdir ~/Documents/Linux/log/data-stream
+fi
+
+access_log=~/Documents/Linux/log/data-stream/access.log
+errors_log=~/Documents/Linux/log/data-stream/errors.log
+
+git=/usr/bin/git
+
+if [ -f $git ]; then
+    git branch 2>> $errors_log 1>> $access_log
+fi
