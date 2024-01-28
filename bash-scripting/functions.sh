c41e12f1b90622ae08b10b38f73ce38f6f58b147
ilyasse-fouaide <ilyasse.fouaide@gmail.com>
2024-01-28 14:53:33 +0100

    update

--- bash-scripting/functions.sh
+++ bash-scripting/functions.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+# basic function
 git_status="$(git status -s)"
 
-check_git_status() {
+check_git_status () {
     if [ -n "$git_status" ]; then
         echo "*"
     else
@@ -11,3 +12,23 @@ check_git_status() {
 }
 
 check_git_status
+
+echo "--------------------------"
+
+# passing argument
+fn () {
+    echo "hello $1, you have $2 years old";
+}
+
+fn "ilyasse" 20;
+
+echo "--------------------------"
+
+# returning value
+fn2 () {
+    echo "hello $1"
+    return 5
+}
+
+fn2 "ilyasse"
+echo The previous function has a return value of $?
