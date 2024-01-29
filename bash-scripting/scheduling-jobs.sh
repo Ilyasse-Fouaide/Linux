2bc4ac3fc88119191afc73c20f2c48cd8df05219
ilyasse-fouaide <ilyasse.fouaide@gmail.com>
2024-01-29 22:04:55 +0100

    updating...

--- bash-scripting/scheduling-jobs.sh
+++ bash-scripting/scheduling-jobs.sh
@@ -3,11 +3,11 @@
 /usr/bin/echo "At $(date)" >> log/job.log
 
 # --- to schedul this script: ---
-# ./scheduling-jobs | at 10:50 AM
+# at 11:50 AM -f ./scheduling-jobs.sh
 
 # --- to check the jobs in queue ---
 # atq
-# output: 3	    Mon Jan 29 11:00:00 2024 a ilyasse
+# output: 3	    Mon Jan 29 11:50:00 2024 a ilyasse
 
 # --- to remove the jobs in queue ---
 # atrm <number_of_queue>
